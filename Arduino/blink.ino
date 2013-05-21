f126cc46e861a32cb561fb9ca45cc0da091172c2
Simon Vanherweghe <simon.vanherweghe@gmail.com>
2013-05-21 21:45:25 +0200

    Files toegevoegd

---
+++ Arduino/blink.ino
@@ -0,0 +1,14 @@
+int counter=0;
+void setup(void) {
+Serial.begin(9600);
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(13, LOW);
+  delay(1000);
+  digitalWrite(13, HIGH);
+  counter++;
+  Serial.println(counter,DEC);
+  delay(1000);
+}
